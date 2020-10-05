0aceec38044a04e743e30455f79d95008cbafd41
Gent Semaj <gentsemaj@live.com>
2020-10-05 11:34:07 -0400

    Implement socket.io

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 var express = require("express");
 var app = express();
-var http = require('http').createServer(app);
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
 });
 
-app.use('/res', express.static('res'));
+app.use("/res", express.static("res"));
 
 http.listen(80, () => {
-  console.log('listening on *:80');
+	console.log("listening on *:80");
+});
+
+io.on("connection", (socket) => {
+	socket.on("poll", () => {
+		console.log("poll requested");
+	});
 });
